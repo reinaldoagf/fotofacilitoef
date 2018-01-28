ed4462754857cdeb58a6becd7afd05417532cf77
reinaldoagf <reinaldoagf1@gmail.com>
2018-01-28 13:45:00 -0430

    Sirviendo archivos estaticos con express.static() y leyendo parametros con body-parser

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express= require('express')
+var bodyParser= require('body-parser')
+var mongoose= require('mongoose')
+
 var app= express()
+
+app.use('/static',express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.set('view engine','jade')
+
 app.get('/',function (request,response) {
 	response.render('layouts/login')
+})
+app.post('/users',function (request,response) {
+	response.send('Datos obtenidos satisfactoriamente')
+	console.log('Email: '+request.body.email)
+	console.log('Password: '+request.body.password)
 })
 app.get('/foto',function (request,response) {
 	response.render('photo/index')
 })
+
 console.log('Server running in https://localhost:80')
 app.listen(80)
